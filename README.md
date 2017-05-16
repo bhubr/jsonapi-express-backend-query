c27d06be02c57e996898de0c8230af5bcdcda264
Benoît Hubert <benoithubert@gmail.com>
2017-05-17 01:58:31 +0200

    Add readme, package metadata, cleanup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wrappers for SQL engines
+
+This motivation for this package is to provide lightweight wrappers for my [JSONAPI Express Backend](npmjs.com/package/jsonapi-express-backend) module.
+
+**It currently works with MySQL only**, with pgsql support underway but not functional yet.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 module.exports = function(config) {
-  if(! config || ['mysql', 'pgsql', 'sqlite'].indexOf(config.dialect) === -1) {
-    throw new Error("config object's dialect key should be one of (mysql|pgsql|sqlite) instead of " + config.dialect);
+  if(! config || ['mysql', 'pgsql'].indexOf(config.dialect) === -1) { // Leave sqlite for later, 'sqlite' |sqlite
+    throw new Error("config object's dialect key should be one of (mysql|pgsql) instead of " + config.dialect);
   }
   const { dialect } = config;
   delete config.dialect;
